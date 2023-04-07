e8fb6cb92c6fbd0e9b477b349df8c7f419c5b33e
bsae19 <99652825+bsae19@users.noreply.github.com>
2023-04-07 17:13:01 +0200

    Update musique_liste.gd

--- menu/musique_liste.gd
+++ menu/musique_liste.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
+var color=preload("res://couleur/black.tres")
 var format=[".mp3"]
+var actuel=null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var files = []
@@ -8,6 +10,7 @@ func _ready():
 	if dir:
 		dir.list_dir_begin()
 		var file_name=dir.get_next()
+		var x=0
 		while file_name !="":
 			if !dir.current_is_dir():
 				for i in format:
@@ -17,10 +20,40 @@ func _ready():
 						var info=file_name.split(i)
 						file2.append(info[0])
 						files.append(file2)
+						var body=StaticBody3D.new()
+						body.position.y=x
+						x-=1
+						add_child(body)
+						var morceau = MeshInstance3D.new()
+						var rec=BoxMesh.new()
+						rec.size.x=1.5
+						rec.size.y=0.5
+						rec.size.z=0.2
+						rec.material=color
+						morceau.mesh=rec
+						morceau.position.x=-1.4
+						morceau.position.y=1.6
+						morceau.scale.x=1.3
+						body.add_child(morceau)
+						var coli=CollisionShape3D.new()
+						var shape=BoxShape3D.new()
+						shape.size.x=0.976
+						shape.size.y=0.25
+						shape.size.z=0.1
+						shape.margin=0.04
+						coli.shape=shape
+						coli.position.x=-1.4
+						coli.position.y=1.6
+						body.add_child(coli)
+						body.connect("mouse_entered",Callable(self, "_on_StaticBody3D_mouse_entered"))
+						#body.connect("mouse_exited",Callable(self, "_on_StaticBody3D_mouse_exited"))
 				file_name=dir.get_next()
 	print(files)
-	pass
 
+
+func _on_StaticBody3D_mouse_entered():
+	var listen=get_signal_connection_list("mouse_entered")
+	print(listen.size())
 #Titre:  Overthinking
 #Auteur: RYYZN
 #Source: https://soundcloud.com/ryyzn
@@ -33,3 +66,4 @@ func _ready():
 
 func _process(_delta):
 	pass
+
